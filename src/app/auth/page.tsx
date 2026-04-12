7642607a98729a45be67c04044b4cc332ecb5bce
Fenix75Hz <lucas@dms-home.com>
2026-04-12 01:38:34 -0300

    chore: remove all files

--- src/app/auth/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AuthShell } from "@/components/auth/auth-shell"
-
-export default function AuthPage() {
-  return <AuthShell />
-}
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { LandingShell } from "@/components/landing/landing-shell"
-
-export default function Home() {
-  return <LandingShell />
-}
